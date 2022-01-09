e2d6c10f1093a49c396c2578347032e0775130d1
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-01-09 18:22:09 +0000

    Add is_32bit and source_all as included functions

---
+++ R/source_all.R
@@ -0,0 +1,24 @@
+#' Source all function.
+#'
+#' Source all R files in a directory at once. 
+#' Also loads selected libraries. 
+#' This function can be useful when developing R packages, 
+#' instead of rebuilding the entire package every time you make a change 
+#' (which is a more robust way to checking the package works, 
+#' but is much slower).
+#' 
+#' @inheritParams list.files
+#' @keywords internal
+source_all <- function(path="R/",
+                       pattern="*.R$",
+                       packages="dplyr"){   
+    for(x in packages){ 
+        library(x, character.only=TRUE)
+    }
+    ### Source all internal funcs at once 
+    file.sources = list.files(path =path,
+                              pattern = pattern, 
+                              full.names = TRUE, ignore.case = TRUE)
+    message("Sourcing ",length(file.sources)," files.")
+    out <- sapply(file.sources,source)
+}
--- tests/testthat/test-messager.R
+++ tests/testthat/test-messager.R
@@ -1,7 +1,11 @@
 test_that("messager works", {
     
-    msg <- "Hello echoverse"
-    msg_out <- utils::capture.output(messager(msg),
-                                     type = "message")
-    testthat::expect_equal(msg, msg_out)
+    if(!is_32bit()){
+        msg <- "Hello world"
+        msg_out <- utils::capture.output(messager(msg),
+                                         type = "message")
+        testthat::expect_equal(msg, msg_out)
+    } else {
+        testthat::expect_null(NULL)   
+    }
 })
