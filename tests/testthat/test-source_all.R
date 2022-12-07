39e1b5dc42fcc24be8f5e98789218807f4e3ade3
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-12-07 17:14:38 +0000

    Remove is_32bit

--- tests/testthat/test-source_all.R
+++ tests/testthat/test-source_all.R
@@ -1,11 +1,7 @@
 test_that("source_all works", {
-  
-    if(!is_32bit()){
-        source_all(path = "../../R", 
-                   packages = "dplyr") 
-        testthat::expect_true(exists("messager"))
-        testthat::expect_true("dplyr" %in% .packages())
-    } else {
-        testthat::expect_null(NULL)
-    } 
+    
+    source_all(path = "../../R", 
+               packages = "stats") 
+    testthat::expect_true(exists("messager"))
+    testthat::expect_true("stats" %in% .packages())
 })
--- tests/testthat/test-stopper.R
+++ tests/testthat/test-stopper.R
@@ -1,10 +1,6 @@
-test_that("stopper works", {
+test_that("stopper works", { 
     
-    if(!is_32bit()){
-        msg <- "You have encountered an error"
-        testthat::expect_error(stopper(msg = msg))
-        testthat::expect_error(stopper(msg = msg, v=FALSE))
-    } else{
-        testthat::expect_null(NULL)
-    }
+    msg <- "You have encountered an error"
+    testthat::expect_error(stopper(msg = msg))
+    testthat::expect_error(stopper(msg = msg, v=FALSE))
 })
